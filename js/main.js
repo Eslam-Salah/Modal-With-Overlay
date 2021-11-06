55e8a8e86989bffef7f7486eac7361a1030940ed
Eslam-Salah <eslamsalah524@gmail.com>
2021-11-06 06:25:56 +0200

    edited

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-let buttons = document.querySelectorAll(".project .btn");
+let overlay = document.querySelectorAll(".overlay");
 let modal = document.querySelector(".modal");
 let modalImage = document.querySelector(".modal-inner img");
 let closeBtn = document.querySelector(".close");
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+overlay.forEach((el) => {
+  el.addEventListener("click", (e) => {
     modal.classList.add("open");
-    let src =
-      e.currentTarget.parentElement.parentElement.querySelector("img").src;
+    let src = e.currentTarget.parentElement.querySelector("img").src;
     modalImage.src = src;
   });
 });
